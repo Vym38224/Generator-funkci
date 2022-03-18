89eec4e94f7d00284e8a5605fa97724557f59e3b
Vym38224 <jardaavymetal@gmail.com>
2022-03-18 22:03:05 +0100

    zacatek2

--- generator funkcí.py
+++ generator funkcí.py
@@ -1,40 +1,89 @@
+#!/usr/bin/env python3
+
+from os.path import basename, splitext
+import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+from tkinter import Entry
 
-sample_rate = 44100
-f = 50   #Hz
-
-t = np.arange(0,1,1/sample_rate)  #Time
+# from tkinter import ttk
 
 
-#normalize function
-def norm(data):
-    min_v = min(data)
-    max_v = max(data)
-    return np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "Generátor funkcí"
+
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lbl = tk.Label(self, text="Generátor funkcí")
+        self.lbl.pack()
+        self.btn2 = tk.Button(self, text="Vygeneruj", command=self.graf)
+        self.btn2.pack()
+        self.btn = tk.Button(self, text="Quit", command=self.quit)
+        self.btn.pack()
+        self.lbl2 = tk.Label(self, text="Nastavení frekvence:")
+        self.lbl2.pack()
+        self.entry = Entry(self)
+        self.entry.pack()
+        self.entry.focus_set()
+
+        def volanafunkce():
+            print(self.entry.get())
+
+        
+            
+
+        self.btn3 = tk.Button(self, text="zapiš", command=volanafunkce)
+        self.btn3.pack()
 
 
-####PRODUCE SIGNALS
-signal = np.sin(2*np.pi*f*t)      #Sinus
-#signal = np.cos(2*np.pi*f*t)       #Cosinus
-#signal = (np.mod(f*t,1) < 0.5)*2.0-1 #Rectangle
+        
+
+    def graf(self):
+        sample_rate = 44100
+        t = np.arange(0,1,1/sample_rate)        #Time
+        f = self.entry.get()          #Hz
+
+        #SIGNALS
+        signal = np.sin(2*np.pi*f*t)            #Sinus
+        #signal = np.cos(2*np.pi*f*t)           #Cosinus
+        #signal = (np.mod(f*t,1) < 0.5)*2.0-1   #Rectangle
+
+        noise = np.random.randn(*signal.shape)
+
+        #COMBINE NOISE AND SIGNAL
+        def norm(data):
+        
+            min_v = min(data)
+            max_v = max(data)
+            return np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
+
+        dirty =  norm(signal+noise)
+
+        #WRITE AUDIO FILE
+        signal = signal * 32767
+        signal = np.int16(signal)
+        wavfile.write("file.wav", sample_rate, signal)
+
+        ####PLOT SIGNAL####
+        plt.plot(t, signal,"black")
+        plt.plot(t, signal+noise,"blue");
+        plt.ylabel("napětí[V]")
+        plt.xlabel("čas[s]")
+        plt.title("Signál")
+        plt.show()
+
+    def quit(self, event=None):
+        super().quit()
 
 
-noise = np.random.randn(*signal.shape)
+app = Application()
+app.mainloop()
 
-####COMBINE NOISE AND SIGNAL
-dirty =  norm(signal+noise)
 
-####WRITE AUDIO FILE
-signal = signal * 32767
-signal = np.int16(signal)
-wavfile.write("file.wav", sample_rate, signal)
 
-####PLOT SIGNAL####
-plt.plot(t, signal,"black")
-plt.plot(t, signal+noise,"blue");
-plt.ylabel("napětí[V]")
-plt.xlabel("čas[s]")
-plt.title("Signál")
-plt.show()
+
+
