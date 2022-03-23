7a09a113fe279275243bb7267c8e96414ebf66d6
Vym38224 <jardaavymetal@gmail.com>
2022-03-23 12:54:43 +0100

    veskole

--- generator funkcí.py
+++ generator funkcí.py
@@ -20,17 +20,11 @@ class Application(tk.Tk):
         self.bind("<Escape>", self.quit)
         self.lbl = tk.Label(self, text="Generátor funkcí")
         self.lbl.pack()
-        self.btn2 = tk.Button(self, text="Vygeneruj", command=self.graf)
-        self.btn2.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
         self.lbl2 = tk.Label(self, text="Nastavení frekvence:")
         self.lbl2.pack()
         self.f = Entry(self)
         self.f.pack()
         self.f.focus_set()
-
-        
 
         def frekvence():
             try:
@@ -43,14 +37,17 @@ class Application(tk.Tk):
             print(self.f.get())
             f1.close()
             
-
-        self.btn3 = tk.Button(self, text="zapiš", command=frekvence)
+        self.btn3 = tk.Button(self, text="Zapiš", command=frekvence)
         self.btn3.pack()
+        self.btn2 = tk.Button(self, text="Vygeneruj", command=self.graf)
+        self.btn2.pack()
+        self.btn = tk.Button(self, text="Konec", command=self.quit)
+        self.btn.pack()
        
 
     def graf(self):
         #TIME
-        sample_rate = 44100
+        sample_rate = 45000
         t = np.arange(0,1,1/sample_rate)
 
         #FREQUENCY        
@@ -60,9 +57,9 @@ class Application(tk.Tk):
         f1.close()
                                            
         #SIGNALS
-        #signal = np.sin(2*np.pi*f*t)            #Sinus
+        signal = np.sin(2*np.pi*f*t)            #Sinus
         #signal = np.cos(2*np.pi*f*t)           #Cosinus
-        signal = (np.mod(f*t,1) < 0.5)*2.0-1   #Rectangle
+        #signal = (np.mod(f*t,1) < 0.5)*2.0-1   #Rectangle
 
         noise = np.random.randn(*signal.shape)
 
@@ -71,21 +68,22 @@ class Application(tk.Tk):
         
             min_v = min(data)
             max_v = max(data)
-            return np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
+            return np.array([((x-min_v) / (max_v-min_v)) for x in data])*2-1
 
         dirty =  norm(signal+noise)
 
         #WRITE AUDIO FILE
-        signal = signal * 32767
+        signal = signal * 15000
         signal = np.int16(signal)
         wavfile.write("file.wav", sample_rate, signal)
 
-        ####PLOT SIGNAL
+        #PLOT SIGNAL
         plt.plot(t, signal,"black")
-        plt.plot(t, signal+noise,"blue");
-        plt.ylabel("napětí[mV]")
+        plt.plot(t, signal+noise,"blue")
+        plt.ylabel("Up-p[mV]")
         plt.xlabel("čas[s]")
         plt.title("Signál")
+        plt.grid()
         plt.show()
 
     def quit(self, event=None):
